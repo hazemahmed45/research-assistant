088381f08ab61f4a20f22ab3b789e3d9d372a43f
hazem <hazem.haroun@brightskiesinc.com>
2025-01-06 18:02:02 +0200

    finalized summary route

--- src/api/routers/summary_router.py
+++ src/api/routers/summary_router.py
@@ -15,24 +15,26 @@ from loguru import logger
 from langchain_community.document_loaders.pdf import PyPDFLoader
 from langchain_core.documents.base import Document
 
-from src.misc.document_schema import DocumentStructureSchema
-from src.models.doc_embedder import BaseDocumentEmbedder
-from src.models.doc_analyizer import BaseDocumentAnalyizer
-from src.models.doc_vectorstore import BaseVectorstore
+
 from src.api.config import ApiSettings
 from src.api.schema import (
     DocumentSummaryInputSchema,
     DocumentSummaryOutputSchema,
 )
-from src.misc.logger_handlers import FileHandler
-from src.misc.create_unique_id import create_unique_user_id
 from src.models import (
     SingletonModelFactory,
     DocumentAnalyizerSettings,
     DocumentEmbedderSettings,
     VectorstoreSettings,
+    BaseDocumentEmbedder,
+    BaseDocumentAnalyizer,
+    BaseVectorstore,
 )
-from src.misc.utils import merge_documents
+from src.database import SingletonDatabaseFactory, DBSettings, BaseDB
+from src.misc.document_schema import DocumentStructureSchema
+from src.misc.logger_handlers import FileHandler
+from src.misc.create_unique_id import create_unique_user_id
+from src.misc.utils import merge_pages_in_document
 from src.enums import Constants
 
 REQUEST_TYPE = "sum-doc"
@@ -54,6 +56,8 @@ document_analyizer: BaseDocumentAnalyizer = factory_results[
 vectorstore: BaseVectorstore = factory_results[
     SingletonModelFactory.FactoryUnpackValues.VECTORSTORE
 ]
+
+database: BaseDB = SingletonDatabaseFactory.build(db_settings=DBSettings())
 
 
 @sum_doc_router.post("/sum_doc", tags=[Constants.SUMMARIZE_DOCUMNET_TAG.name])
@@ -83,20 +87,27 @@ async def summarize_doc(
     session_logger.info("Request Recieved")
 
     # TODO IMPLEMENT SUMMARIZE DOCUMENTS ROUTE
-
+    session_logger.info(f"Reading document pages from {schema.link}")
     pdf_loader = PyPDFLoader(file_path=schema.link)
     document_pages: List[Document] = pdf_loader.load()
-    document: Document = merge_documents(documents=document_pages)
+    session_logger.info(f"Loaded {len(document_pages)} pages")
+    document: Document = merge_pages_in_document(document_pages=document_pages)
+    session_logger.info(f"Merged all pages into one document with id {document.id}")
 
+    session_logger.info("Start document analyizing")
     document_structured_extraction: DocumentStructureSchema = document_analyizer(
         doc_context=document
     )
-    print(vectorstore.add_structured_document(document_structured_extraction))
+    session_logger.info("Finished document analyizing")
     background_tasks.add_task(
         vectorstore.add_structured_document,
         structured_document=document_structured_extraction,
     )
+    background_tasks.add_task(
+        database.push_document, structured_document=document_structured_extraction
+    )
     background_tasks.add_task(session_logger.remove)
+
     return DocumentSummaryOutputSchema(
         document_summary_extraction=document_structured_extraction
     )
